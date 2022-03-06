4843aaf32844bada23be56598790fed9cb11036e
suhyeon21 <suhyeon.kan@gmail.com>
2022-03-06 14:03:33 +0900

    feat: Make search function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDehXvwhtZrC-eEzi8qIcb5niionm-hbZw`,
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId })),
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -12,14 +30,19 @@ function App() {
     };
 
     fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyDehXvwhtZrC-eEzi8qIcb5niionm-hbZw",
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDehXvwhtZrC-eEzi8qIcb5niionm-hbZw",
       requestOptions,
     )
       .then((response) => response.json())
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
