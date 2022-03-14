a6fd5b91d581af065d7466153578de29a4e83878
suhyeon <suhyeon.kan@gmail.com>
2022-03-14 19:53:12 +0900

    feat: show selected video and the list

--- src/app.jsx
+++ src/app.jsx
@@ -6,15 +6,18 @@ import VideoList from "./components/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo]  = useState(null);
-  
-  const selectVideo=(video)=>{
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
     setSelectedVideo(video);
-  }
+  };
   const search = (query) => {
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
@@ -25,10 +28,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      {
-        selectedVideo && <VideoDetail video={selectedVideo} />
-      }
-      <VideoList videos={videos} onVideoClick={selectVideo}/>
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
